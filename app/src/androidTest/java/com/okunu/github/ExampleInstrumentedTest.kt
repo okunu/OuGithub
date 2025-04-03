534d7e7d8162a0d9c15bb3559d47c7e1b4753211
okunu <okunu@qq.com>
2025-04-03 14:12:52 +0800

    更新一个ui的单元测试用例

--- app/src/androidTest/java/com/okunu/github/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/okunu/github/ExampleInstrumentedTest.kt
@@ -1,12 +1,20 @@
 package com.okunu.github
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.okunu.github.ui.main.MainActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,6 +23,17 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testClickUserMenuItemNavigatesToLoginPage() {
+        onView(withId(R.id.user))
+            .perform(click())
+        onView(withId(com.okunu.login.R.id.login_icon))
+            .check(matches(isDisplayed()))
+    }
     @Test
     fun useAppContext() {
         // Context of the app under test.
